c19eab3f1b241aa74f7470e815e372494ba93222
CapitaineToinon <asutter@wideagency.com>
2020-08-19 16:12:57 +0200

    Published the docs folder

---
+++ docs/main.js
@@ -0,0 +1,1 @@
+export{V as default}from"./main-77453a5e.js";
---
+++ docs/onResize-25edceed.js
@@ -0,0 +1,1 @@
+import{o as n}from"./main-77453a5e.js";function e(e,o){window.addEventListener(e,o,!1),n((function(){return window.removeEventListener(e,o,!1)}))}function o(n){return e("resize",n)}export{e as a,o};
